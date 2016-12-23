c0509ab48a72a5992e9a6f4dd683b80547d6d2d2
Margriet Palm <m.m.palm@lacdr.leidenuniv.nl>
2016-12-23 13:53:07 +0100

    Added initial code base to repo

---
+++ README.md
@@ -0,0 +1,110 @@
+# Visualizing 3D grid data
+
+## Installation
+
+### Dependencies
+- Numpy
+- Matplotlib
+- VTK Python bindings
+- im2movie script (link)
+
+### Set up as command line program
+1. Make script executable:
+    ```chmod +x visualizer.py```
+2. Create a link to `visualizer` in a folder in your `PATH`. For example, when you have a `bin` folder in your home
+ that is in your `PATH`: `ln -s /path/to/VisGrid3D/visualizer.py /home/USERNAME/bin/visgrid3D`
+3. Now you can run the script with `visgrid3D`
+
+### Python library
+1. Install dependencies (see above)
+2. Place `VisGrid3D` folder in the python path
+
+
+## Generating VTK files in Morpheus
+Morpheus can generate VTK files with the *VtkPlotter* analysis plugin. Set up this plugin to write ascii files and
+ to store the cell id and cell type:
+
+```
+<VtkPlotter mode="ascii" time-step="10">
+	<Channel symbol-ref="cell.id"/>
+	<Channel symbol-ref="cell.type"/>
+</VtkPlotter>
+```
+
+Note that VTK files can become quite large. So keep the number of output files low during testing, or remove them.
+
+
+## Usage
+To animate the results you must at least specify the folder containing the vtk files (`-i`) and the cell types
+to animate (`-t`). All other command line arguments are optimal and use defaults if not specify.
+
+### Examples
+
+- Plot cell types 0 and 2 for results in `morphesu/3d_migration_138/`:
+
+```animate_morpheus_3D -i morpheus/3d_migration_138/ -t 0 2```
+
+- Colors and opacity can be defined per cell type:
+
+```animate_morpheus_3D -i morpheus/3d_migration_138/ -t 0 2 -c red grey -a 1 0.1```
+
+- Static (frozen) cell types can be defined such that they are only rendered once:
+
+```animate_morpheus_3D -i morpheus/3d_migration_138/ -t 0 2 -c red grey -a 1 0.1 --static 2```
+
+- Save snapshots and make movie
+
+```animate_morpheus_3D -i morpheus/3d_migration_138/ -t 0 2 -c red grey -a 1 0.1 --static 2 -s -m```
+
+
+### Help
+
+```
+$ animate_morpheus_3D -h
+usage: animate_morpheus_3D [-h] -i SIMDIR [-w WINSIZE WINSIZE] -t
+                           [CELLTYPES [CELLTYPES ...]]
+                           [-c [COLORS [COLORS ...]]] [-a [ALPHA [ALPHA ...]]]
+                           [--static [STATIC [STATIC ...]]]
+                           [--bboxcolor BBOXCOLOR BBOXCOLOR BBOXCOLOR]
+                           [--bgcolor BGCOLOR BGCOLOR BGCOLOR]
+                           [--camposition CAMPOSITION CAMPOSITION CAMPOSITION]
+                           [--camfocus CAMFOCUS CAMFOCUS CAMFOCUS]
+                           [--steps [STEPS [STEPS ...]]] [-f FPS] [-o OUTDIR]
+                           [-p IMPREFIX] [-s] [-m] [--win] [--mp4]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i SIMDIR, --simdir SIMDIR
+                        Simulation folder
+  -w WINSIZE WINSIZE, --winsize WINSIZE WINSIZE
+                        window size
+  -t [CELLTYPES [CELLTYPES ...]], --celltypes [CELLTYPES [CELLTYPES ...]]
+                        cell types to animate
+  -c [COLORS [COLORS ...]], --colors [COLORS [COLORS ...]]
+                        colors or the cell types
+  -a [ALPHA [ALPHA ...]], --alpha [ALPHA [ALPHA ...]]
+                        opacity of the cell types
+  --static [STATIC [STATIC ...]]
+                        static cell types (will NOT be updated during
+                        animation)
+  --bboxcolor BBOXCOLOR BBOXCOLOR BBOXCOLOR
+                        bounding box color
+  --bgcolor BGCOLOR BGCOLOR BGCOLOR
+                        background color
+  --camposition CAMPOSITION CAMPOSITION CAMPOSITION
+                        camera position
+  --camfocus CAMFOCUS CAMFOCUS CAMFOCUS
+                        camera focal point
+  --steps [STEPS [STEPS ...]]
+                        steps to animate, all steps will be shown if this is
+                        not specified
+  -f FPS, --fps FPS     frames per second
+  -o OUTDIR, --outdir OUTDIR
+                        output directory
+  -p IMPREFIX, --imprefix IMPREFIX
+                        image prefix
+  -s, --saveim          save images
+  -m, --movie           make movie
+  --win                 make movie windows compatible
+  --mp4                 make mp4 movie
+```
---
+++ visualizer.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+"""Visualizes data on a cubic lattice
+
+Built specifically to visualize the VTK files created by Morpheus
+"""
+
+import os
+import glob
+import sys
+import argparse
+
+import numpy as np
+import vtk
+from vtk.util import numpy_support as VN
+from matplotlib import colors
+
+found_im2movie = True
+try:
+    from im2movie import makeMovie
+except ImportError:
+    found_im2movie = False
+
+__author__ = "Margriet Palm"
+__copyright__ = "Copyright 2016"
+__credits__ = "Margriet Palm"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Margriet Palm"
+
+
+# the vtkTimerCallback takes care of updating the visualzation
+class vtkTimerCallback():
+    def __init__(self, update_func, tmax=1):
+        self.timer_count = 0
+        self.update = update_func
+        self.tmax = tmax
+        self.update_actors = None
+
+    def execute(self, obj, event):
+        iren = obj
+        win = iren.GetRenderWindow()
+        ren = win.GetRenderers().GetFirstRenderer()
+        # remove all actors that will be updated
+        for actor in self.update_actors:
+            ren.RemoveActor(actor)
+        # set t to correct value
+        t = self.timer_count
+        if self.timer_count >= self.tmax:
+            t = self.timer_count % self.tmax
+            self.save = False
+        # get new actors
+        actors = self.update(t)
+        self.update_actors = actors
+        self.timer_count += 1
+
+
+class Visualizer3D():
+    def __init__(self, simdir, steps=None, winsize=(800, 800), bg=(0, 0, 0), bbox_color=(1, 1, 1),
+                 cam_props=None):
+        """
+        :param simdir: path to folder containing vtk files
+        :param steps: steps to visualize
+        :param winsize: window size
+        :param bg: background color
+        :param bbox_color: bounding box wire frame color
+        :param cam_props: dictionary with camera settings
+        """
+        self.bbox_color = bbox_color
+        self.cam_props = cam_props
+        # read data
+        files = glob.glob('{}/plot_*.vtk'.format(simdir))
+        get_num = lambda fn: int(fn.split('_')[-1].replace('.vtk', ''))
+        if steps is not None:
+            self.data = {get_num(f): self._get_data(f) for f in files if get_num(f) in steps}
+        else:
+            self.data = {get_num(f): self._get_data(f) for f in files}
+        # setup renderer
+        self._set_renderer(winsize, bg)
+
+    def _set_renderer(self, winsize, bg):
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(bg[0], bg[1], bg[2])
+        self.renderWindow = vtk.vtkRenderWindow()
+        self.renderWindow.AddRenderer(self.renderer);
+        self.renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+        self.renderWindowInteractor.SetRenderWindow(self.renderWindow)
+        self.renderWindow.SetSize(winsize[0], winsize[1])
+
+    def get_actors(self, step, tau_list, tau_colors=None, tau_alpha=None, bbox=True):
+        """
+        Create actors for a list of cell types and add them to the renderer
+
+        :param step: step to visualize
+        :param tau_list: list of cell types
+        :param tau_colors: list with color per cell type
+        :param tau_alpha: list with opacity per cell type
+        :param bbox: show bounding box
+        """
+        # set default colors and opacity when they are not specified
+        if tau_colors is None:
+            tau_colors = [(0.5, 0.5, 0.5) for tau in tau_list]
+        if tau_alpha is None:
+            tau_alpha = [1 for tau in tau_list]
+        # get actors
+        actors = [self._get_actor_for_tau(step, tau, tau_colors[i], tau_alpha[i]) for i, tau in enumerate(tau_list)]
+        # get bounding box wire frame
+        if bbox:
+            actors.append(self._get_box_actor())
+        # add actors to the renderer
+        for actor in actors:
+            self.renderer.AddActor(actor)
+        return actors
+
+    def _modify_cam(self):
+        """ Modify the camera settings for the renderer.
+
+        Available options:
+            - position
+            - focal point
+            - pitch
+
+        If position and focal point are not given, they will be taken
+        from the camera in the renderer.
+
+        :param renderer: vtk renderer
+        :param cam_props: dictionary with options (see above) as keys and settings as values
+        """
+        old_cam = self.renderer.GetActiveCamera();
+        cam = vtk.vtkCamera()
+        if 'position' in self.cam_props:
+            cam.SetPosition(self.cam_props['position'])
+        else:
+            cam.SetPosition(old_cam.GetPosition())
+        if 'focal_point' in self.cam_props:
+            cam.SetFocalPoint(self.cam_props['focal point'])
+        else:
+            cam.SetFocalPoint(old_cam.GetFocalPoint())
+        if 'pitch' in self.cam_props:
+            cam.Pitch(self.cam_props['pitch'])
+        self.renderer.SetActiveCamera(cam)
+
+    def _get_box_actor(self):
+        (w, h, d) = self.data[self.data.keys()[0]].GetDimensions()
+        imageData = vtk.vtkImageData()
+        imageData.SetDimensions(2, 2, 2)
+        imageData.SetSpacing(w, h, d)
+        imageData.SetOrigin(0, 0, 0)
+        mapper = vtk.vtkDataSetMapper()
+        mapper.SetInputData(imageData)
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(self.bbox_color[0], self.bbox_color[1], self.bbox_color[2])
+        actor.GetProperty().SetRepresentationToWireframe()
+        return actor
+
+    def _get_actor_for_tau(self, step, show_tau, color=(0.5, 0.5, 0.5), opacity=1):
+        if isinstance(color, basestring):
+            # convert color to rgb string
+            if color in colors.cnames:
+                color = get_color(color)
+            else:
+                color = (0.5, 0.5, 0.5)
+        dim = self.data[step].GetDimensions()
+        sigma = VN.vtk_to_numpy(self.data[step].GetPointData().GetArray('cell.id'))
+        sigma = sigma.reshape(dim, order='F')
+        tau = VN.vtk_to_numpy(self.data[step].GetPointData().GetArray('cell.type'))
+        tau = tau.reshape(dim, order='F')
+        show_idx = np.unique(sigma[tau == show_tau])
+        points = vtk.vtkPoints()
+        for s in show_idx:
+            if s not in sigma:
+                continue
+            pix = np.column_stack(np.where(sigma == s))
+            for p in pix:
+                points.InsertNextPoint(p[0] - .5, p[1] - .5, p[2] - .5)
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
+        sources = vtk.vtkCubeSource()
+        sources.Update()
+        glyph = vtk.vtkGlyph3D()
+        glyph.SetInputData(polydata)
+        glyph.SetSourceConnection(sources.GetOutputPort())
+        glyph.ScalingOff()
+        glyph.Update()
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(glyph.GetOutputPort())
+        actor = vtk.vtkActor()
+        actor.GetProperty().SetOpacity(opacity)
+        actor.GetProperty().SetColor(color[0], color[1], color[2])
+        actor.SetMapper(mapper)
+        return actor
+
+    def _get_data(self, fn):
+        # load data
+        reader = vtk.vtkStructuredPointsReader()
+        reader.SetFileName(fn)
+        reader.ReadAllScalarsOn()
+        reader.Update()
+        return reader.GetOutput()
+
+    def visualize(self, step, tau_list, show=False, save=False, impath=None, imprefix=None, bbox=True,
+                  tau_alpha=None, tau_colors=None):
+        """
+        Visualize a given step.
+
+        :param step: step to visualize
+        :param tau_list: list of cell types
+        :param show: initialize and start the render window after adding the actors to the renderer,
+        should not be used for animations
+        :param save: save view to png
+        :param impath: path to store image
+        :param imprefix: image prefix
+        :param bbox: show bounding box
+        :param tau_alpha: list with opacity per cell type
+        :param tau_colors: list with color per cell type
+        :return:
+        """
+        self.renderWindow.SetWindowName('step ' + str(int(step)))
+        actors = self.get_actors(step, tau_list, tau_colors, tau_alpha, bbox=bbox)
+        self.renderWindow.Render()
+        if self.cam_props is not None:
+            self._modify_cam()
+        if show:
+            self.renderWindowInteractor.Initialize()
+            self.renderWindowInteractor.Start()
+        if save:
+            w2i = vtk.vtkWindowToImageFilter()
+            w2i.SetInput(self.renderWindow)
+            w2i.Update()
+            writer = vtk.vtkPNGWriter()
+            writer.SetInputConnection(w2i.GetOutputPort())
+            if imprefix is not None and imprefix.endswith('_'):
+                imprefix = imprefix + '_'
+            if imprefix is None:
+                imprefix = ''
+            if impath is None:
+                impath = '.'
+            writer.SetFileName('{}/{}{:03d}.png'.format(impath, imprefix, step))
+            print '{}/{}{:03d}.png'.format(impath, imprefix, step)
+            writer.Write()
+        return actors
+
+    def animate(self, tau, tau_colors=None, tau_alpha=None, steps=None, save=False, impath=None, imprefix=None,
+                fps=5, static_tau=None):
+        """
+        Animate simulation results
+
+        :param tau: list of cell types
+        :param tau_colors: list with color per cell type
+        :param tau_alpha: list with opacity per cell type
+        :param steps: steps (all steps are shown when not specified)
+        :param save: save view to png
+        :param impath: path to store image
+        :param imprefix: image prefix
+        :param fps: frames per second
+        :param static_tau: static cell types that should not be updated during the animation
+        :return:
+        """
+        if (tau_colors is None) or (len(tau_colors) is not len(tau)):
+            tau_colors = [(.5, .5, .5) for t in tau]
+        if (tau_alpha is None) or (len(tau_alpha) is not len(tau)):
+            tau_alpha = [1 for t in tau]
+        if steps is None:
+            steps = self.data.keys()
+        steps.sort()
+        self.renderWindowInteractor.Initialize()
+        actors = self.visualize(0, tau, show=False, save=False, bbox=True, tau_alpha=tau_alpha,
+                                tau_colors=tau_colors)
+        if static_tau is None:
+            static_tau = tau
+        update_tau = [t for t in tau if t not in static_tau]
+        update_colors = [tau_colors[i] for i, t in enumerate(tau) if t in update_tau]
+        update_alpha = [tau_alpha[i] for i, t in enumerate(tau) if t in update_tau]
+        update_func = lambda t: self.visualize(t, update_tau, show=False, save=save, bbox=False,
+                                               tau_alpha=update_alpha, tau_colors=update_colors)
+        cb = vtkTimerCallback(update_func, len(steps))
+        cb.update_actors = [actors[0]]
+        self.renderWindowInteractor.AddObserver('TimerEvent', cb.execute)
+        timerId = self.renderWindowInteractor.CreateRepeatingTimer(int(1000 / float(fps)))
+        cb.timerId = timerId
+        # start the interaction and timer
+        self.renderWindowInteractor.Start()
+
+
+def get_color(name):
+    cc = colors.ColorConverter()
+    if name in colors.cnames:
+        return cc.to_rgb(name)
+    else:
+        return cc.to_rgb("grey")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # parser.description("Animate 3D Morpheus simulations")
+    parser.add_argument("-i", "--simdir", type=str, default="./", help="Simulation folder", required=True)
+    parser.add_argument("-w", "--winsize", type=int, nargs=2, help="window size", default=(800, 800))
+    parser.add_argument("-t", "--celltypes", type=int, nargs="*", help="cell types to animate", required=True)
+    parser.add_argument("-c", "--colors", type=str, nargs="*", help="colors or the cell types")
+    parser.add_argument("-a", "--alpha", type=float, nargs="*", help="opacity of the cell types")
+    parser.add_argument("--static", type=int, nargs="*",
+                        help="static cell types (will NOT be updated during animation)")
+    parser.add_argument("--bboxcolor", type=float, nargs=3, default=(1, 1, 1), help="bounding box color")
+    parser.add_argument("--bgcolor", type=float, nargs=3, default=(0, 0, 0), help="background color")
+    parser.add_argument("--camposition", type=float, nargs=3, default=(-200, 200, 200), help="camera position")
+    parser.add_argument("--camfocus", type=float, nargs=3, default=(100, 100, 50), help="camera focal point")
+    parser.add_argument("--steps", type=int, nargs="*", help="steps to animate, all steps will be shown if this "
+                                                             "is not specified")
+    parser.add_argument("-f", "--fps", type=float, default=5, help="frames per second")
+    parser.add_argument("-o", "--outdir", type=str, help="output directory")
+    parser.add_argument("-p", "--imprefix", type=str, help="image prefix")
+    parser.add_argument("-s", "--saveim", action="store_true", help="save images")
+    parser.add_argument("-m", "--movie", action="store_true", help="make movie")
+    parser.add_argument("--win", action="store_true", help="make movie windows compatible")
+    parser.add_argument("--mp4", action="store_true", help="make mp4 movie")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    # check if there is something to animate
+    if not os.path.isdir(args.simdir):
+        sys.exit("Could not find {}".format(args.simdir))
+    elif len(glob.glob("{}/*.vtk".format(args.simdir))) == 0:
+        sys.exit("No vtk files found in {}".format(args.simdir))
+
+    # set colors and opacity
+    if not args.colors:
+        print "Cell color not specified - default to grey"
+        args.colors = [get_color("grey") for t in args.celltypes]
+    elif len(args.colors) == 1:
+        args.colors = [get_color(args.colors) for t in args.celltypes]
+    elif len(args.colors) < len(args.celltypes):
+        print "Number of colors does not match number of cell types - default to grey"
+        args.colors = [get_color("grey") for t in args.celltypes]
+    else:
+        args.colors = [get_color(c) for c in args.colors]
+    if not args.alpha:
+        print "Alpha values not specified - default to opaque objects"
+        args.alpha = [1 for t in args.celltypes]
+    elif len(args.alpha) == 1:
+        args.alpha = [args.alpha for t in args.celltypes]
+    elif len(args.alpha) < len(args.celltypes):
+        print "Number of alpha values does not match number of cell types - default to opaque objects"
+        args.alpha = [1 for t in args.celltypes]
+
+    # set saving options
+    if args.imprefix or args.outdir or args.movie:
+        args.saveim = True
+    if args.saveim:
+        if not args.outdir:
+            args.outdir = args.simdir
+        if not os.path.isdir(args.outdir):
+            print "Create output directory {}".format(args.outdir)
+            os.makedirs(args.outdir)
+        if not args.imprefix:
+            args.imprefix = "frame"
+
+    # set camera
+    cam_props = {'position': args.camposition, 'focal point': args.camfocus}
+    # create visualizer
+    v = Visualizer3D(args.simdir, winsize=args.winsize, bg=args.bgcolor, bbox_color=args.bboxcolor,
+                     cam_props=cam_props)
+    # start animation
+    v.animate(args.celltypes, tau_colors=args.colors, tau_alpha=args.alpha, steps=args.steps,
+              save=args.saveim, impath=args.outdir, imprefix=args.imprefix, fps=args.fps, static_tau=args.static)
+    # create and store movie
+    if args.movie and found_im2movie:
+        makeMovie(args.imprefix, '.png', args.imprefix, args.outdir, args.outdir, args.fps,
+                  win=args.win, tomp4=args.mp4)
+    elif not found_im2movie:
+        print "WARNING: Movie generation is turned of because im2movie was not found"
+
+
+if __name__ == "__main__":
+    main()
